114f807992768c1415cbe23daf06c51c96af2b04
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2014-09-28 08:29:22 -0400

    Updated apps & app component added support for app creation

--- client/js/components/app.js
+++ client/js/components/app.js
@@ -4,11 +4,12 @@ var React = require('react');
 
 var AppComponent = React.createClass({
   render: function() {
+    var app=this.props.app.attributes;
     return (
-      <li key={this.props.app.id}>
-      <div><span>App Name: </span><span>{this.props.app.name}</span></div>
-      <div><span>App Key: </span><span>{this.props.app.key}</span></div>
-      <div><span>App Secret: </span><span>{this.props.app.secret}</span></div>
+      <li key={app.id}>
+      <div><span>App Name: </span><span>{app.name}</span></div>
+      <div><span>App Key: </span><span>{app.api_key}</span></div>
+      <div><span>App Secret: </span><span>{app.api_secret}</span></div>
       </li>
     );
   }
--- client/js/components/apps.js
+++ client/js/components/apps.js
@@ -1,22 +1,58 @@
 /** @jsx React.DOM */
 
-var React = require('react');
+var $ = require('jquery'),
+    React = require('react'),
+    Backbone = require('backbone'),
+    AppComponent = require('./app');
+
+var AppModel = Backbone.Model.extend({
+});
+
+var AppsCollection = Backbone.Collection.extend({
+  model: AppModel,
+  url: "/applications",
+  createApp: function(appName){
+    this.create({name: appName},{wait: true});
+  }
+});
+
+var NewAppComponent = React.createClass({
+  handleKeyDown: function(e){
+    if(e.keyCode!==13) return;
+    var appName=e.target.value;
+    this.props.apps.createApp(appName);
+    e.target.value='';
+  },
+  render: function(){
+    return <li>
+    <input type="text" placeholder="enterAppName" onKeyDown={this.handleKeyDown}/>
+    </li>
+  }
+});
 
 var AppsComponent = React.createClass({
+  getInitialState: function () {
+    return {apps: new AppsCollection()};
+  },
   componentWillMount: function() {
     var self = this;
-    $.getJSON('/applications', function(data) {
-      self.setState({apps: data});
-    });
+    this.refresh();
+    this.state.apps.on('reset',this.update,this);
+    this.state.apps.on('add',this.update,this);
+  },
+  refresh: function(){ this.state.apps.fetch({reset: true}) },
+  update: function(){
+    this.setState({apps: this.state.apps});
   },
   render: function() {
     var appNodes = this.state.apps.map(function (app) {
       return (
-        <App app={app}/>
+        <AppComponent app={app}/>
       );
     });
     return <ul>
       {appNodes}
+      <NewAppComponent apps={this.state.apps}/>
     </ul>;
   }
 });
