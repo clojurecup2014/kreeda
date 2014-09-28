5dc07038e7da67f9695e5d3cbb0b303440831846
Vagmi Mudumbai <vagmi.mudumbai@gmail.com>
2014-09-28 16:34:00 +0530

    refactor entites add routes to list and add applications

---
+++ src/kreeda/controllers/applications.clj
@@ -0,0 +1,26 @@
+(ns kreeda.controllers.applications
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [noir.response :as resp]
+            [kreeda.models.application :as app-model]
+            [kreeda.auth :as auth]))
+
+(defn index-route [req]
+  (let [current-user (auth/current-user req)
+        applications (app-model/by-customer (:id current-user))]
+    (resp/json (into [] applications))))
+
+(defn create-route [req]
+  (let [current-user (auth/current-user req)
+        app-name (get-in req [:body-params :application :name])]
+    (resp/json (app-model/create app-name (:id current-user)))))
+
+(defroutes applications-routes
+  (GET "/applications" request (auth/authorize (index-route request)))
+  (POST "/applications" request (auth/authorize (create-route request)))
+  ;(GET "/applications/:id" [request] (auth/authorize (show-route request)))
+  ;(POST "/applications/:id" [request] (auth/authorize (update-route request)))
+  ;(DELETE "/applications/:id" [request] (auth/authorize (destroy-route request)))
+  )
+
+(def app (auth/wrap-authentication applications-routes))
--- src/kreeda/models/customer.clj
+++ src/kreeda/models/customer.clj
@@ -1,19 +1,12 @@
 (ns kreeda.models.customer
   (:require [korma.db :refer :all]
             [korma.core :refer :all]
-            [clj-time.core :as t]
             [clojure.data.json :as json]
+            [kreeda.models.utils :refer :all]
+            [kreeda.models.application :as application]
+            [kreeda.models.entities :refer :all]
             [kreeda.models.base :refer [db]]))
 
-(declare customers customer-identity)
-(defn now [] (java.sql.Date. (.getMillis ^org.joda.time.DateTime (t/now))))
-(defentity customer
-  (table :customers)
-  (has-many customer-identity {:fk :customer_id}))
-
-(defentity customer-identity
-  (table :identities)
-  (belongs-to customer {:fk :customer_id}))
 
 (defn by-provider [provider provider-key provider-meta]
   (let [customers (select customer-identity
@@ -22,16 +15,12 @@
     (if (empty? customers)
       (let [{:keys [login email]} provider-meta
             cust (insert customer 
-                             (values {:email email :name (:name provider-meta) 
-                                      :created_at (now) 
-                                      :updated_at (now)}))]
-
+                         (values (timestamps! {:email email 
+                                               :name (:name provider-meta)})))]
         (insert customer-identity
-                (values {:provider "github" :provider_key login
-                         :created_at (now) 
-                         :updated_at (now)
+                (values (timestamps! {:provider "github" :provider_key login
                          :provider_meta (json/write-str provider-meta)
-                         :customer_id (:id cust)}))
+                         :customer_id (:id cust)})))
         (by-provider "github" login {}))
       customers)))
 
