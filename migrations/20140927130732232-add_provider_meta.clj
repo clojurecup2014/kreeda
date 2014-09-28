e5fd46c3313acb1457eadeb65a4830b953d898b1
Dhruva Sagar <dhruva.sagar@gmail.com>
2014-09-28 13:13:18 +0530

    Updated add_provider_meta migration

--- migrations/20140927130732232-add_provider_meta.clj
+++ migrations/20140927130732232-add_provider_meta.clj
@@ -1,7 +1,7 @@
 ;; migrations/20140927130732232-add_provider_meta.clj
 
 (defn up []
-  ["create extension hstore", "alter table identities add column provider_meta text"])
+  ["alter table identities add column provider_meta text"])
 
 (defn down []
   ["alter table identities drop column provider_meta"])
